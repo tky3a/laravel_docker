bb3fc394338d78ad7fd25c2941848005a71be54b
tky3a <takayamac@icloud.com>
2021-02-28 22:09:29 +0900

    グループ別ルーティング

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -22,9 +22,22 @@ Route::get('/', function () {
 // 最新Laravelの書き方
 Route::get('tests/test', [TestController::class, 'index']);
 
-Route::resource('contacts', ContactFormController::class)->only([
-    'index', 'show'
-]);
+// Route::resource('contacts', ContactFormController::class)->only([
+//     'index', 'show'
+// ]);
+
+//左の'contact/index'部分でルーティング
+// 右の'index'の部分で対応するコントローラーメソッド
+// Route::get('contact/index', [ContactFormController::class, 'index']);
+// Route::get('contact/create', [ContactFormController::class, 'create']);
+// Route::get('contact/show', [ContactFormController::class, 'create']);
+// Route::get('contact/create', [ContactFormController::class, 'create']);
+
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function () {
+    Route::get('index', [ContactFormController::class, 'index'])->name('contact.index');
+    Route::get('create', [ContactFormController::class, 'create'])->name('contact.create');
+    Route::post('store', [ContactFormController::class, 'store'])->name('contact.store');
+});
 
 Auth::routes();
 
